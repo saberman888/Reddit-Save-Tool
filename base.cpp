2f18ffa90f2e67f071b0610151306a42ca6277a3
Sai <saberman8881@gmail.com>
2019-09-07 05:03:07 -0600

    Removed an unnecessary function

--- base.cpp
+++ base.cpp
@@ -9,18 +9,6 @@ size_t writedat(char* buffer, size_t size, size_t nmemb, std::string& src)
 	return size * nmemb;
 }
 
-std::vector<std::string> split_string(std::string src, const char delimeter)
-{
-	std::vector<std::string> tokens;
-	std::string token;
-	std::istringstream tokenStream(src);
-	while (std::getline(tokenStream, token, delimeter))
-	{
-		tokens.push_back(token);
-	}
-	return tokens;
-}
-
 bool _Item::IsVideo()
 {
 	return (is_video || domain.rfind("v.redd.it", 0) != std::string::npos || url.rfind("gifv", 0) != std::string::npos);
