35bc34c620a08cad35755e4ed23c674428c0892b
Sai <saberman8881@gmail.com>
2019-09-07 05:11:10 -0600

    Update Saver.hpp
    Saver now scans argv for command line parameters rather than scan_cmd being its own function in Source
    Wrapped __GNUC__ in defined()
    AccessSaved merged with AccessPosts
    Saver now scans the command line arguments instead of cmd_args being it's own function in Source.cpp
    AccessPosts now reads from args for whether or not comments should be retrieved

--- Saver.hpp
+++ Saver.hpp
@@ -16,50 +16,29 @@
 #include <map>
 #ifdef _MSC_VER
 #include <filesystem>
-#elif __GNUC__ && !(defined(__MINGW64__) || defined(__MINGW32__))
+#elif defined(__GNUC__) && !(defined(__MINGW64__) || defined(__MINGW32__))
 #include <experimental/filesystem>
 #else
 #include <filesystem>
 #endif
 #include "Reddit.hpp"
 
-
-// State holds the results of a request
-
-
-
 class Saver : public RedditAccess
 {
 public:
-	Saver(CMDArgs* arg);
-	~Saver(){}
-	/* these toggles enable using old Reddit or printing out the linked urls with the saved urls
-	   by default old Reddit and outputting linked urls are disabled */
-
-
+	Saver();
+	bool scan_cmd(int argc, char* argv[]);
 	State RetrieveComments(Item *i);
-	State AccessPosts(std::vector<Item*>& saved, bool get_comments);
+	State AccessPosts(std::vector<Item*>& saved);
 	void WriteLinkCSV(std::vector<Item*> src) { std::vector<std::string> filter; write_links(src, filter); }
 	void WriteLinkCSV(std::vector<Item*> src, std::vector<std::string> filter) { write_links(src, filter); }
 	bool write_links(std::vector<Item*> src, std::vector<std::string> subfilter);
+	void download_content(std::vector<Item*> i, Sort s);
 	bool posts_only;
 	bool comments_only;
 
-	// Some stats I want the program to collect while cycling through the items
-	int images, self_posts, links, videos, comments;
-
 private:
-	/*
-		SaveToggle saves or unsaved depending if remove is enabled or not
-	
-	*/
-	State SaveToggle(std::string fullname, bool remove);
-	// Just gets the user's save items
-	State get_saved_items(std::vector< Item* >& sitem, std::string after, bool prev_continue, bool get_comments);
-	State retrieve_comments(Item* i);
-	// AccessSaved is the function to retrieve saved items, and they're stored in std::vector<Item*>& saved
-	State AccessSaved(std::vector<Item*>& saved, std::string after, bool prev_continue, bool get_comments);
-public:
-	void download_content(std::vector<Item*> i, Sort s);
 
+	State get_saved_items(std::vector< Item* >& sitem, std::string after, bool get_comments);
+	State retrieve_comments(Item* i);
 };
