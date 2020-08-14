0876bc353f221c88ad858d965500f6b39ca823d1
saberman888 <sagittariusz548@gmail.com>
2020-08-14 11:04:53 -0600

    Revert "Removed includes for unistd,sys/typesm pwd and synchapi"
    This reverts commit 08c87e1b149d87e57116dc4d6dd959fea98c9d9e.

--- Reddit.hpp
+++ Reddit.hpp
@@ -1,11 +1,16 @@
 #pragma once
 #include <string>
 #include "base.hpp"
+#include <chrono>
+#include <ctime>
 #include <fstream>
 #include "BasicRequest.hpp"
-#include "base.hpp"
 #include "nlohmann/json.hpp"
 #include "curl/curl.h"
+
+constexpr int POST_LIMIT = 1000;
+constexpr int LIMIT_PER_TOKEN = 600;
+constexpr int RQ_PER_MINUTE = 60;
 
 #if defined(__cpp_lib_filesystem)
 	#include <filesystem>
@@ -22,30 +27,42 @@
 
 
 #if defined(unix) || defined(_unix)
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 constexpr bool IsUnixBased = true;
 #else
 constexpr bool IsUnixBased = false;
+#include <Synchapi.h>
 #endif
 
 class RedditAccess {
 public:
 	RedditAccess();
-	// obtain_token gets the access token from Reddit*/
-	State AccessReddit() { return this->obtain_token(); }
-	State RedditGetRequest(std::string endpoint, std::string& buffer);
-	bool IsLoggedIn;
-	fs::path MediaPath;
-	BasicRequest RedditHandle;
+	bool LoadLogins();
+	/*obtain_token gets the access token from Redditand refreshes
+		for a new one when refresh is true*/
+	State AccessReddit() { return this->obtain_token(false); }
+	State RefreshToken() { return this->obtain_token(true); }
 
+	bool IsLoggedIn;
+	CMDArgs args;
+	fs::path StoragePath;
+
+private:
 	// Where we're going to store user information when we parse it from the config file
 	struct
 	{
-		std::string Token;
 		std::string Username, Password;
 		std::string ClientId, Secret, UserAgent;
 	} UserAccount;
 
-private:
-	State obtain_token();
-	std::string ReadJson(std::string json);
+	struct
+	{
+		int TotalPosts;
+		std::string after;
+		std::vector<Item> posts;
+	};
+
+	State obtain_token(bool refresh);
 };
