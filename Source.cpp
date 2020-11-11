7e086ef82be50524d766a2438368096e69642120
saberman888 <sagittariusz548@gmail.com>
2020-11-10 22:17:11 -0700

    Update TODOs

--- Source.cpp
+++ Source.cpp
@@ -8,13 +8,10 @@ using namespace std;
  *
  * TODO: Add filtering options for subreddit, nsfw and more
  * TODO: Add getting gifv
- * TODO: Finish adding in Imgur suppoer
  * TODO: Allow ffmpeg command to be changed using the settings file
  * TODO: Add folder organization options for subreddit, nsfw and types.
  * TODO: Add option to keep video or audio file if getting one of them fails
  * TODO: In BasicRequest change response from being a pointer to a normal variable
- * TODO: Add error checking if critical arguments aren't present like --account
- * TODO: Add getting Reddit galleries
  * TODO: Try to fix getting video
  */
 int main(int argc, char* argv[])
